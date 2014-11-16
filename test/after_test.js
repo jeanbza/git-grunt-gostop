f8f01adba144e37cbb021049a6694d37d9c82921
Jean de Klerk <jadekler@gmail.com>
2014-11-15 19:39:17 -0700

    Before and after tests 'fixed' (they now wait a second before checking, to give system time to clean up)

--- test/after_test.js
+++ test/after_test.js
@@ -13,12 +13,14 @@ exports.gostop = {
             path: '/foo'
         };
 
-        http.get(options, function(res) {
-            test.equal(false, true, 'Server should have been stopped - should not be able to curl it');
-            test.done();
-        }).on('error', function(e) {
-            test.equal(true, true, 'Server successfully killed');
-            test.done();
-        });
+        setTimeout(function() {
+            http.get(options, function(res) {
+                test.equal(false, true, 'Server should have been stopped - should not be able to curl it');
+                test.done();
+            }).on('error', function(e) {
+                test.equal(true, true, 'Server successfully killed');
+                test.done();
+            });
+        }, 1000);
     }   
 };
